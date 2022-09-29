8bbaf00c95ad9a42314208b53cd86109191e766d
Michael Sepsey <msepsey@umich.edu>
2022-09-29 17:16:04 -0400

    Master corrects a compiler error on Hello.java

--- Hello.java
+++ Hello.java
@@ -2,11 +2,11 @@ public class Hello {
  public static void main(String args[]) {
    for (int i = 1; i <= 10; i++)
    {
-     system.out.println("Hello World!"+i);
-     system.out.println("This line added by master branch"+i);
-     system.out.println("This line added by master branch"+i);
-     system.out.println("Master tells you to say hi to others"+i);
-     system.out.println("Master hopes you have a good day"+i);
+     System.out.println("Hello World!"+i);
+     System.out.println("This line added by master branch"+i);
+     System.out.println("This line added by master branch"+i);
+     System.out.println("Master tells you to say hi to others"+i);
+     System.out.println("Master hopes you have a good day"+i);
    }
  }
 }
